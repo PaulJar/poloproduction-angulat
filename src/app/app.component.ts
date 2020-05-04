09bf059620dccf14f843def5494ad28f2b0cf55d
Paul Jardel <paul.jardel@accenture.com>
2020-05-04 16:34:48 +0200

    Added : authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'poloproduction-angular';
+  constructor() {
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyAzm71y1ZjKLbW7N0bYcWqzSTK2ewWCykA",
+      authDomain: "polo-production.firebaseapp.com",
+      databaseURL: "https://polo-production.firebaseio.com",
+      projectId: "polo-production",
+      storageBucket: "polo-production.appspot.com",
+      messagingSenderId: "326901133147",
+      appId: "1:326901133147:web:e0eea4478c35a345c8747e",
+      measurementId: "G-W8W33KSYQQ"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  }
 }
